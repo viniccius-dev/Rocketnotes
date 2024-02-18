ef6d57c5c6903c97a662b9a4cb281293133e34b4
Vinícius <santsmarcos81@gmail.com>
2024-02-18 18:15:45 -0300

    Criando migrates de tags e links

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require("express")
+
+const notesRoutes = Router()
+
+const NotesController = require('../controllers/NotesController') //VScode pointing out non-existent error in the correct path which would be: '../controllers/UsersController'
+
+const notesController = new NotesController()
+
+notesRoutes.post("/:user_id", notesController.create) //If used, middleware for a specific route
+
+module.exports = notesRoutes
