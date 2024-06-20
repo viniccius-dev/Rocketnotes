fa813675e44740a904a9f761aa16476553eb8bcf
Vinícius <santsmarcos81@gmail.com>
2024-06-20 12:11:30 -0300

    Describe

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,16 +1,28 @@
 const UserCreateService = require('./UserCreateService');
 const UserRepositoryInMemory = require('../repositories/UserRepositoryInMemory');
 
-it("user should be create", async () => {
-    const user = {
-        name: 'User Test',
-        email: 'user@test.com',
-        password: "123"
-    };
-
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
-    const userCreated = await userCreateService.execute(user);
+describe("UserCreateService", () => {
+    it("user should be create", async () => {
+        const user = {
+            name: 'User Test',
+            email: 'user@test.com',
+            password: "123"
+        };
     
-    expect(userCreated).toHaveProperty("id");
+        const userRepositoryInMemory = new UserRepositoryInMemory();
+        const userCreateService = new UserCreateService(userRepositoryInMemory);
+        const userCreated = await userCreateService.execute(user);
+        
+        expect(userCreated).toHaveProperty("id");
+    });
+
+    it("another test", () => {
+        expect(1).toBe(2);
+    });
 });
+
+describe("NotesCreateService", () => {
+    it("create notes", async () => {
+        expect(1).toBe(1);
+    })
+})
